9128126fc61931f8c51fc1601a7a4a2ba688e69c
ForesterSilver <lesnovda@gmail.com>
2025-01-19 18:26:27 +0300

    version 2 (with rolling)

--- roll_a_dice.rb
+++ roll_a_dice.rb
@@ -1,6 +1,14 @@
 puts 'How many dice?'
 number = gets.to_i
 
+counter = 5
+while counter != 0
+  puts "rolling #{rand(6)+1}"
+  sleep 0.5
+  counter = counter - 1
+  system "cls"
+end
+
 # С помощью мтеода целого числа times запускаем цикл, который заданное число раз
 # повторяет бросок кубика.
 number.times do
