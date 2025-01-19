000fb5057d55bca360aa252b3d4986e5e2e0034d
ForesterSilver <lesnovda@gmail.com>
2025-01-19 18:47:21 +0300

    version 3

--- roll_a_dice.rb
+++ roll_a_dice.rb
@@ -1,5 +1,7 @@
 puts 'How many dice?'
 number = gets.to_i
+rand = 0
+sum = 0
 
 counter = 5
 while counter != 0
@@ -12,5 +14,9 @@ end
 # С помощью мтеода целого числа times запускаем цикл, который заданное число раз
 # повторяет бросок кубика.
 number.times do
-  puts rand(6) + 1
+  rand = rand(6) + 1
+  puts rand
+  sum += rand
 end
+
+puts "Sum of dice #{sum}"
